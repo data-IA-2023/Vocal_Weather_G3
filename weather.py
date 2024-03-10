3a815b7f7956b41a267281c67dd30b3829916323
comewlecome <mohasoft@gmail.com>
2024-03-10 19:53:09 +0100

    MVP1

--- weather.py
+++ weather.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, abort, Response
 import urllib
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 
 from voice import recognize_from_microphone
 from camembert import NLP
@@ -15,24 +16,32 @@ app = Flask(__name__)
 
 @app.route('/forecast', methods=['GET'])
 def get_weather():
-    city = request.args.get('city')
-    if city is None:
-        abort(400, 'Missing argument city')
+    city = request.args.get('ville')
+    date_str = request.args.get('date')
+    
+    if city is None or date_str is None:
+        abort(400, 'Missing argument city or date')
+    
+    try:
+        date = datetime.strptime(date_str, '%Y-%m-%d')
+    except ValueError:
+        abort(400, 'Invalid date format. Please use YYYY-MM-DD')
     
     data = {}
     data['q'] = city
     data['appid'] = os.environ['METEOKEY']
     data['units'] = 'metric'
+    data['dt'] = int(date.timestamp())  # Convert date to Unix timestamp
     
     url_values = urllib.parse.urlencode(data)
     url = 'http://api.openweathermap.org/data/2.5/forecast'
     full_url = url + '?' + url_values
-    data = urllib.request.urlopen(full_url)
+    response = urllib.request.urlopen(full_url)
     
-    resp = Response(data)
+    resp = Response(response)
     resp.status_code = 200
-    return render_template('index.html', title='Weather App', data=json.loads(data.read().decode('utf8')))
-
+    titre= f'La météo à {city} le {date_str} est'
+    return render_template('index.html', titre=titre, data=json.loads(response.read().decode('utf8')))
 
 @app.route('/speech-to-text', methods=['post'])
 def speechToText():
@@ -44,8 +53,8 @@ def decode():
     phrase = request.args.get('speech_text')
     data= NLP(phrase=phrase)
     ville = data.get('ville', [''])[0]
-    
-    return render_template('index.html', ville = ville)
+    date = data.get('date', [''])[0]
+    return render_template('index.html', ville = ville, date= date)
 
 @app.route('/')
 def index():
